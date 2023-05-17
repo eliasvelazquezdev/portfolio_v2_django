66279e7a4faa59edf6dba06181c0d3061c2e2778
Elias Velazquez <evelazquez2604@gmail.com>
2023-05-17 16:44:38 -0300

    Añade plantillas y partials. Añade middleware para responder con plantilla segun resolucion de pantalla

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from mobi2.decorators import detect_mobile
 
 # Create your views here.
+@detect_mobile
+def index(request):
+    if not request.mobile:
+        return render (request, 'index.html')
+    else:
+        return render (request, 'index_mobile.html')
+
+
+def thanks(request):
+    return render(request, 'thanks.html')
